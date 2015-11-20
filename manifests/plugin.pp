ccf4a7ad8c3bf76a172a2414c491ae4e15305325
Foudil BRÉTEL <foudil.bretel@cc.in2p3.fr>
2015-11-20 12:12:01 +0100

    Add `fetch` parameter to plugins.
    This will use wget instead of maven to fetch plugins.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -21,38 +21,64 @@ define sonarqube::plugin(
   $ensure     = present,
   $artifactid = $name,
   $groupid    = 'org.codehaus.sonar-plugins',
+  $fetch      = {},
 ) {
+  validate_hash($fetch)
+
   $plugin_name = "${artifactid}-${version}.jar"
-  $plugin      = "${sonarqube::plugin_dir}/${plugin_name}"
+  $plugin_path = "${sonarqube::plugin_dir}/${plugin_name}"
 
   # Install plugin
   if $ensure == present {
-    # copy to a temp file as Maven can run as a different user and not have rights to copy to
-    # sonar plugin folder
-    maven { "/tmp/${plugin_name}":
-      groupid    => $groupid,
-      artifactid => $artifactid,
-      version    => $version,
-      before     => File[$plugin],
-      require    => File[$sonarqube::plugin_dir],
-    }
-    ~>
+
     exec { "remove-old-versions-of-${artifactid}":
       command     => "/tmp/cleanup-old-plugin-versions.sh ${sonarqube::plugin_dir} ${artifactid} ${version}",
       path        => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin',
-      refreshonly => true,
+      refreshonly => true
     }
-    ->
-    file { $plugin:
-      ensure => $ensure,
-      source => "/tmp/${plugin_name}",
-      owner  => $sonarqube::user,
-      group  => $sonarqube::group,
-      notify => Service['sonarqube'],
+
+    if empty($fetch) {
+
+      # copy to a temp file as Maven can run as a different user and not have
+      # rights to copy to sonar plugin folder
+      maven { "/tmp/${plugin_name}":
+        groupid    => $groupid,
+        artifactid => $artifactid,
+        version    => $version,
+        notify     => Exec["remove-old-versions-of-${artifactid}"],
+        require    => File[$sonarqube::plugin_dir],
+      }
+      ->
+      file { $plugin_path:
+        ensure => $ensure,
+        source => "/tmp/${plugin_name}",
+        owner  => $sonarqube::user,
+        group  => $sonarqube::group,
+        notify => Service['sonarqube'],
+      }
+
+    } else {
+
+      if !(has_key($fetch, 'base_url') and has_key($fetch, 'source_hash')) {
+        fail("parameter 'fetch' must have keys 'base_url' and 'source_hash'")
+      }
+      $base_url    = $fetch['base_url']
+      $source_hash = $fetch['source_hash']
+
+      wget::fetch { $artifactid:
+        source      => "${base_url}/${plugin_name}",
+        source_hash => $source_hash,
+        destination => $plugin_path,
+        execuser    => $sonarqube::user,
+        notify => [Exec["remove-old-versions-of-${artifactid}"],
+                   Service['sonarqube']],
+      }
+
     }
+
   } else {
     # Uninstall plugin if absent
-    file { $plugin:
+    file { $plugin_path:
       ensure => $ensure,
       notify => Service['sonarqube'],
     }
